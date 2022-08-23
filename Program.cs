0fdf808676ad30e54dc1109f9fdd131698ce2712
Jusmasterr <111336147+Jusmasterr@users.noreply.github.com>
2022-08-23 10:27:21 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Number_Guesser
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rd = new Random();
+            int Number = rd.Next(1, 100);
+            int Guesses = 0;
+            Console.WriteLine("Guess the Number between 1 and 100:");
+            bool Guessing = true;
+            while (true)
+            {
+                int guessed_n = Convert.ToInt32(Console.ReadLine());
+                if (Number == guessed_n)
+                {
+                    Guesses++;
+                    Console.WriteLine("Great you guessed the right Number after " + Guesses + " attempts" );
+                    Guessing = false;
+                }
+                else
+                {
+                    Guesses++;
+                    if (Number > guessed_n)
+                    {
+                        Console.WriteLine("The number is bigger");
+                    }
+                    else
+                    {
+                        Console.WriteLine("The Number is smaller");
+                    }
+                }
+            }
+        }
+    }
+}
